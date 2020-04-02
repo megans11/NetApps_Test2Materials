d9be7f1f11dd75844668d3ab4b679a699cfd30dd
megans11 <40861278+megans11@users.noreply.github.com>
2020-04-02 08:20:41 -0400

    Add files via upload

--- testCode/multiprocessing_import_worker.py
+++ testCode/multiprocessing_import_worker.py
@@ -1,6 +1,13 @@
 # multiprocessing_import_worker.py
 
-def worker():
+import pickle
+
+def worker(payload):
     """worker function"""
-    print('Worker')
+    answer_payload = pickle.loads(payload)
+    sumPayload = 0
+    for i in answer_payload[1]:
+        sumPayload = sumPayload + i
+        
+    print('Worker', answer_payload[0], '----', sumPayload)
     return
